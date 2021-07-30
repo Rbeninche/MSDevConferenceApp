31c25d1122da70991d16c624fa4749c68f89287e
Rbeninche <rbeninche@gmail.com>
2021-07-30 14:46:21 -0400

    Initial commit

---
+++ MSDevConferenceApp/Models/Repository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSDevConferenceApp.Models
+{
+    public static class Repository
+    {
+        private static List<WebminarInvite> responses = new List<WebminarInvite>();
+
+        public static IEnumerable<WebminarInvite> Responses => responses;
+
+        public static void AddResponse(WebminarInvite response)
+        {
+            responses.Add(response);
+        }
+    }
+}
---
+++ MSDevConferenceApp/Models/WebminarInvite.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSDevConferenceApp.Models
+{
+    public class WebminarInvite
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please enter whether you will attend or not")]
+        public bool? WillJoin { get; set; }
+    }
+}
